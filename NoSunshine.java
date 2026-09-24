587fd83a4947deeb386e77499b2492ebd7512f94
gavm09 <gavinrmcg@gmail.com>
2026-09-24 12:51:01 -0700

    Update NoSunshine.java

--- NoSunshine.java
+++ NoSunshine.java
@@ -1,44 +1,13 @@
 public class NoSunshine {
    public static void main (String[] args){
-   String b="Ain't no sunshine when she's gone";
-   String c="Anytime she goes away";
+   String b="Ain't no sunshine when she's gone\n";
+   String c="Anytime she goes away\n";
    String d="I know, ";
    String e=d.repeat(5);
-   a(b);
-   a("It's not warm when she's away");
-   a(b);
-   a("And she's always gone too long");
-   a(c);
-   a("");
-   a("wonder this time where she's gone");
-   a("Wonder if she's gone to stay");
-   a(b);
-   a("And this house just ain't no home");
-   a(c);
-   a("");
-   a("And "+d+d+d+d);
-   a(e);
-   a(e);
-   a(e);
-   a(e);
-   a(d+d);
-   a("");
-   a("Hey, I ought to leave young thin alone");
-   a("But ain't no sunshine when she's gone, whoa-whoa");
-   a(b);
-   a("");
-   a("Only darkness every day");
-   a(b);
-   a("And this house just ain't no home");
-   a("");
-   a(c);
-   a(c);
-   a(c);
-   a(c);
-  
+   String f="\n";
+   a(b+"It's not warm when she's away"+f+b+"And she's always gone too long"+f+c+f+"Wonder this time where she's gone"+f+"Wonder if she's gone to stay"+f+b+"And she's always gone too long"+f+c+f+"And "+d+d+d+d+f+e+f+e+f+e+f+e+f+d+d+f+f+"Hey, I ought to leave young thin alone"+f+"But ain't no sunshine when she's gone, whoa-whoa"+f+b+"Only darkness every day"+f+f+b+"And this house just ain't no home"+f+f+c+c+c+c);  
    }
    public static void a(String h){
-   System.out.println(h);
-   
+   System.out.println(h);   
 }
 }
