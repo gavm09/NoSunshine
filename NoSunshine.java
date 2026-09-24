4fffecb2e6863b07b06325cba5f3ad2f8d8af9a3
gavm09 <gavinrmcg@gmail.com>
2026-09-24 12:54:56 -0700

    eoigneopgin

--- NoSunshine.java
+++ NoSunshine.java
@@ -2,7 +2,7 @@ public class NoSunshine {
    public static void main (String[] args){
    String b="Ain't no sunshine when she's gone\n";
    String c="Anytime she goes away\n";
-   String d="I know, ";
+   String d= "I know, ";
    String e=d.repeat(5);
    String f="\n";
    a(b+"It's not warm when she's away"+f+b+"And she's always gone too long"+f+c+f+"Wonder this time where she's gone"+f+"Wonder if she's gone to stay"+f+b+"And she's always gone too long"+f+c+f+"And "+d+d+d+d+f+e+f+e+f+e+f+e+f+d+d+f+f+"Hey, I ought to leave young thin alone"+f+"But ain't no sunshine when she's gone, whoa-whoa"+f+b+"Only darkness every day"+f+f+b+"And this house just ain't no home"+f+f+c+c+c+c);  
